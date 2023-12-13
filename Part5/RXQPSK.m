7d180c6e8b4a57f747d856f396ea615535673306
Grant Brown <grantb91@gmail.com>
2023-12-13 15:50:38 -0700

    part 5 working

--- Part5/RXQPSK.m
+++ Part5/RXQPSK.m
@@ -27,22 +27,33 @@ xbbRF=2*exp(-i*(2*pi*(fc+Dfc)*t-phic)).*xRF;
 pR=pT;    
 xBB=conv(xbbRF,pT);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Examine Spectral Content of y %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure('Name', 'CTFT of xBB')
+spec_analysis(xBB,1/Ts)
+title('CTFT of xBB')
+fontsize(16,"points")
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Find Timing Phase %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-for tau=[0:4*L]
+j=1
+for tau=[10*L:15*L]
     %p_t(tau+1)=mean(abs(xBB(L+tau:L:length(xBB)-L)).^2);
-    p_t(tau+1)=mean(abs(xBB(500+tau:L:end-500)).^2);
+    p_t(j)=mean(abs(xBB(500+tau:L:500+tau+10*L)).^2);
+    j=j+1;
 end
 figure('Name', 'Ensamble Power of xBB')
 plot(p_t)
 title('Ensamble Power of xBB')
 fontsize(16,"points")
-[M, payload_start] = max(p_t);
-payload_start=min(payload_start)-1;
-
-xBBd=xBB(payload_start:L:end);
+[M, packet_start] = max(p_t);
+packet_start = packet_start - roundn(packet_start,2);
+if packet_start < 0
+    packet_start = packet_start + 100;
+end
+xBBd=xBB(packet_start:L:end);
 
 %%%%%%%%%%%%%%%%%%%%%%
 % DECIMATION         %
@@ -71,6 +82,7 @@ title('Cross-Correlation Graph for Pilot Detection')
 fontsize(16,"points")
 [M, I] = maxk(abs(ryy), 4);
 I = (max(I)+N-1)*L;
+%I = (max(I)+N)*L;
 %I=(max(I)+N)*L -payload_start;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -81,29 +93,47 @@ Ly=length(xBB);
 kk=1;
 yp=0;ym=0;
 start=1;
-tau=0.1*ones(1,floor((Ly-start)/L));
+tau=0.001*ones(1,floor((Ly-start)/L));
 % for k=I+payload_start:L:length(tau)*L-1
 %     tauTb=round(tau(kk)*L); %Moves sample offset to sample value in discrete time
 %     sk(kk)=slicer(xBB(k+tauTb),4); %moves it to closest constellation point
 %     tau(kk+1)=tau(kk)+mu*real(conj(sk(kk)-xBB(k+tauTb))*(xBB(k+tauTb+dtau)-xBB(k+tauTb-dtau)));
 %     kk=kk+1;
 % end
-
-for k=I+payload_start:L:length(tau)*L-L
+test_xBBd = xBBd(137:end);
+for k=I:L:length(tau)*L+L
     tauTb=round(tau(kk)*L);
-    if k+tauTb > length(xBB)
+    tauTB_array(kk)=tauTb;
+    if k+tauTb+dtau > length(xBB)
         break
     end
+    test_xBB(kk)=xBB(k+tauTb);
     sk(kk)=slicer(xBB(k+tauTb),4);
+    error = real(sk(kk)-xBB(k+tauTb));
     tau(kk+1)=tau(kk)+mu*real((sk(kk)-xBB(k+tauTb))*(xBB(k+tauTb+dtau)-xBB(k+tauTb-dtau))');
     kk=kk+1;
 end
-tau_final=tau(end);
-I=round(abs(tau_final*L));
 figure, axes('position',[0.1 0.25 0.8 0.5]), plot(tau(1:kk-1),'k')
 xlabel('Iteration Number, n'), ylabel('\tau[n]')
+plot(xBBd(137:end));
 
+for k=1:length(test_xBBd)
+    test_slicer(k) = slicer(test_xBBd(k), 4);
+end
+test_slicer=test_slicer.';
 xBBd=sk;
+sk=sk.';
+
+index=1;
+for k=1:length(sk)
+    if sk(k) == test_slicer(k)
+        continue
+    else
+        mismitch_index(index)=k;
+        index = index + 1;
+    end 
+end
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Examine Spectral Content of y %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -130,4 +160,7 @@ fontsize(16,"points")
 
 info_bits = QPSK2bits(xBBd);
 data = bin2file(info_bits , 'Part5_Output.txt');
+
+info_bits = QPSK2bits(test_xBBd);
+data = bin2file(info_bits , 'Part5_Output_Test.txt');
 
