e0e04da0a23f72d3ef76c7a1b426e6cb42ee3cbe
Grant Brown <grantb91@gmail.com>
2023-12-14 18:21:47 -0700

    added mse

--- mse.m
+++ mse.m
@@ -1,8 +1,8 @@
-function [mean_squared_error] = mse(symbols)
+function mean_squared_error = mse(symbols)
     mean_squared_error = 0;
     for k=1:length(symbols)
-        sliced_symbol = slicer(symbol, 4);
-        mean_squared_error = mean_squared_error + (sliced_symbol-symbol)^2;
+        sliced_symbol = slicer(symbols(k), 4);
+        mean_squared_error = mean_squared_error + (sliced_symbol-symbols(k))^2;
     end
-    mean_sqaured_error = mean_squared_error/length(symbols);
+    mean_squared_error = mean_squared_error/length(symbols);
 end
